55807ad594a8d3d3ae05379a34bd7b14b25e2bc2
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-26 11:05:14 -0700

    styled card added

---
+++ src/components/StyledCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  align-items: flex-start;
+  background-color: white;
+  border-radius: 10px;
+  filter: drop-shadow(0 0 0.25rem black);
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 10px;
+  width: ${props => props.width || "300px"};
+`;
+
+const Card = props => {
+  return <StyledDiv>{props.children}</StyledDiv>;
+};
+
+export default Card;
