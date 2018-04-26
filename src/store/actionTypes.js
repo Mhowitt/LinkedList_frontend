107c187a2967eab9d38acff7a27fb090695352f2
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-26 16:02:21 -0700

    starting company redux

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -2,3 +2,4 @@ export const SET_CURRENT_USER = "SET_CURRENT_USER";
 export const SET_CURRENT_COMPANY = "SET_CURRENT_COMPANY";
 export const ADD_ERROR = "ADD_ERROR";
 export const REMOVE_ERROR = "REMOVE_ERROR";
+export const GET_COMPANY = "GET_COMPANY";
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import currentUser from "./currentUser";
 import currentCompany from "./currentCompany";
+import companies from "./companies";
 import errors from "./errors";
 
 const rootReducer = combineReducers({
   currentUser,
   currentCompany,
+  companies,
   errors
 });
 
